f71b110fabbcde1cbd019661b8dd633d6066ea8d
stuart <stuartmarshall@fixie3d.com>
2023-03-17 10:47:06 +0000

    Working version with UI

--- main.js
+++ main.js
@@ -12,16 +12,26 @@ const results = document.querySelector('#results')
 const playerScoreText = document.querySelector('.playerScore')
 const computerScoreText = document.querySelector('.computerScore')
 const start = document.querySelector('.start')
+let computerUI = document.querySelector('#computerPick')
 
-start.addEventListener('click', playGame);
+start.addEventListener('click', () =>{
+    for (i=0;i<buttons.length-1;i++){
+        buttons[i].className = "";
+    }
+    
+    document.getElementById('rock').classList.add('btn-choice', 'btn-rock')
+    document.getElementById('paper').classList.add('btn-choice', 'btn-paper')
+    document.getElementById('scissors').classList.add('btn-choice', 'btn-scissors')
+    playGame();
+});
 
 function playGame() {
     start.textContent = 'Reset';
     computerScore = 0;
     playerScore = 0;
     buttons.forEach(button => button.addEventListener('click', clickButton))
-    playerScoreText.textContent = "Player: " + playerScore;    
-    computerScoreText.textContent = "Computer: " + computerScore;  
+    playerScoreText.textContent = playerScore;    
+    computerScoreText.textContent = computerScore;  
     results.textContent = "Let the game begin!"; 
 }
 
@@ -32,8 +42,8 @@ function clickButton(e){
     }
     const playerPick = e.explicitOriginalTarget.id;
     results.textContent = playRound(playerPick, getComputerChoice())
-    playerScoreText.textContent = "Player: " + playerScore;
-    computerScoreText.textContent = "Computer: " + computerScore;
+    playerScoreText.textContent = playerScore;
+    computerScoreText.textContent = computerScore;
     checkWinner(playerScore, computerScore);
 }
 
@@ -51,16 +61,29 @@ function gameScore(winner){
 
 function checkWinner(playerScore, ComputerScore)
 {
+    let gameOver = false;
+
     if(playerScore === 5)
     {
         buttons.forEach(button => button.removeEventListener('click', clickButton))
         results.textContent = "You have won!!";
+        stopButtons();
     }
     else if(computerScore === 5)
     {
         buttons.forEach(button => button.removeEventListener('click', clickButton))
         results.textContent = "You have lost!";
+        stopButtons();
+    }    
+}
+
+function stopButtons() {
+    for (i=0;i<buttons.length-1;i++){
+        buttons[i].className = "";
     }
+    document.getElementById('rock').classList.add('btn-choice', 'rock-plain')
+    document.getElementById('paper').classList.add('btn-choice', 'paper-plain')
+    document.getElementById('scissors').classList.add('btn-choice', 'scissors-plain')
 }
 
 function playRound(playerSelection, computerSelection)
@@ -134,6 +157,22 @@ function getComputerChoice()
     const computerChoice = ["rock", "paper", "scissors"]
     let randomPick = Math.floor(Math.random() * 3)
     let computerPick = computerChoice[randomPick]
+
+    if (computerPick == "rock") {
+        computerUI.className = ""
+        computerUI.classList.add("rock-plain")
+        computerUI.classList.add("btn-choice")
+    }
+    else if (computerPick == "paper") {
+        computerUI.className = ""
+        computerUI.classList.add("paper-plain")
+        computerUI.classList.add("btn-choice")
+    }
+    if (computerPick == "scissors") {
+        computerUI.className = ""
+        computerUI.classList.add("scissors-plain")
+        computerUI.classList.add("btn-choice")
+    }
 
     return computerPick;
 }
